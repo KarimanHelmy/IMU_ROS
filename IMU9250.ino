f990e88339ed2d723da6401969d4d0e238c5cb07
Kariman Ahmed <35960303+KarimanHelmy@users.noreply.github.com>
2021-03-31 18:45:44 +0200

    Add files via upload

--- IMU9250.ino
+++ IMU9250.ino
@@ -6,22 +6,33 @@
 
 #include <Wire.h>
 
-MPU9250 imu(Wire,0x68);
+MPU9250 IMU(Wire,0x68);
+double accX, accY, accZ, gyrX, gyrY, gyrz;
 
 ros::NodeHandle nh;
 sensor_msgs::Imu imu_msg;
 ros::Publisher imu_data("/imu_data", &imu_msg);
 
 uint32_t seq;
+int status;
 
 
 void read_IMU(){
   imu_msg.header.frame_id= "/imu";
-  imu.readSensor();
-  imu_msg.linear_acceleration.x= imu.getAccelX_mss();
-  imu_msg.linear_acceleration.y = imu.getAccelY_mss(); 
-  imu_msg.linear_acceleration.z = imu.getAccelZ_mss();
-  Serial.print(imu_msg.linear_acceleration.x);
+  IMU.readSensor();
+  accX= IMU.getAccelX_mss();
+  accY = IMU.getAccelY_mss(); 
+  accZ= IMU.getAccelZ_mss();
+  imu_msg.linear_acceleration.x= accX;
+  imu_msg.linear_acceleration.y = accY; 
+  imu_msg.linear_acceleration.z = accZ;
+ 
+  imu_msg.angular_velocity.x=IMU.getGyroX_rads();
+  imu_msg.angular_velocity.y=IMU.getGyroY_rads();
+  imu_msg.angular_velocity.z=IMU.getGyroZ_rads();
+
+  Serial.println(imu_msg.angular_velocity.z);
+
 
   imu_data.publish(&imu_msg);
   
@@ -30,10 +41,23 @@ void setup() {
   nh.initNode();
   Wire.begin(); 
   Serial.begin(115200);
+  while(!Serial) {}
 
-      nh.advertise(imu_data);
-
+  // start communication with IMU 
+  status = IMU.begin();
+  if (status < 0) {
+    Serial.println("IMU init/8ialization unsuccessful");
+    Serial.println("Check IMU wiring or try cycling power");
+    Serial.print("Status: ");
+    Serial.println(status);
+    while(1) {}
+    
+  }
+  else{
+    nh.advertise(imu_data);
     read_IMU();
+  }
+ 
   
 }
 
